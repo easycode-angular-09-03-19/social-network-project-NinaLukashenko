ca8ca0754bac19b8f0f7878dbccc92aa25e94746
NinaLukashenko <nina.lucashenco@gmail.com>
2019-05-21 17:51:13 +0300

    Cleaning files

--- src/app/common/modules/directives-module.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DirectivesModuleDirective } from './directives-module.directive';
-
-describe('DirectivesModuleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DirectivesModuleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
